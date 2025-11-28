90ec4b8404ccca1b6e0b95ed243b745378fd1bab
Arravind <aravindram024@gmail.com>
2025-11-28 16:55:54 +0530

    added footer

---
+++ assets/js/logo-carousal.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+	new Splide(".logo-carousal-one", {
+		type: "loop",
+		perPage: 9,
+		gap: "1.5rem",
+		drag: false,
+		pagination: false,
+		arrows: false,
+		autoScroll: {
+			speed: 2,
+			pauseOnHover: false,
+			pauseOnFocus: false,
+		},
+	}).mount(window.splide.Extensions);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	new Splide(".logo-carousal-two", {
+		direction: "rtl",
+		type: "loop",
+		perPage: 9,
+		gap: "1.5rem",
+		drag: false,
+		pagination: false,
+		arrows: false,
+		autoScroll: {
+			speed: 1.5,
+			pauseOnHover: false,
+			pauseOnFocus: false,
+		},
+	}).mount(window.splide.Extensions);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	new Splide(".logo-carousal-three", {
+		type: "loop",
+		perPage: 9,
+		gap: "1.5rem",
+		drag: false,
+		pagination: false,
+		arrows: false,
+		autoScroll: {
+			speed: 1,
+			pauseOnHover: false,
+			pauseOnFocus: false,
+		},
+	}).mount(window.splide.Extensions);
+});
